3d164388d033d0bdfc784ba128de9a6dc5326980
a1260157543 <1260157543@qq.com>
2016-09-26 21:30:35 +0800

    Android嵌入Web页面以及页面的缓存处理
    哈哈哈

---
+++ app/src/main/java/cn/a1260157543/webviewdemo/MainActivity.java
@@ -0,0 +1,100 @@
+package cn.a1260157543.webviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String APP_CACAHE_DIRNAME = "/webcache";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.wv);
+        initWebView();
+        clearWebViewCache();
+        mWebView.loadUrl("https://a1260157543.github.io");
+
+    }
+
+    private void initWebView() {
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);	//设置 缓存模式
+        // 开启 DOM storage API 功能
+        mWebView.getSettings().setDomStorageEnabled(true);
+        //开启 database storage API 功能
+        mWebView.getSettings().setDatabaseEnabled(true);
+        String cacheDirPath = getFilesDir().getAbsolutePath()+APP_CACAHE_DIRNAME;
+//		String cacheDirPath = getCacheDir().getAbsolutePath()+Constant.APP_DB_DIRNAME;
+        Log.i(TAG, "cacheDirPath="+cacheDirPath);
+        //设置数据库缓存路径
+        mWebView.getSettings().setDatabasePath(cacheDirPath);
+        //设置  Application Caches 缓存目录
+        mWebView.getSettings().setAppCachePath(cacheDirPath);
+        //开启 Application Caches 功能
+        mWebView.getSettings().setAppCacheEnabled(true);
+    }
+
+    /**
+     * 清除WebView缓存
+     */
+    public void clearWebViewCache(){
+
+        //清理Webview缓存数据库
+        try {
+            deleteDatabase("webview.db");
+            deleteDatabase("webviewCache.db");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //WebView 缓存文件
+        File appCacheDir = new File(getFilesDir().getAbsolutePath()+APP_CACAHE_DIRNAME);
+        Log.e(TAG, "appCacheDir path="+appCacheDir.getAbsolutePath());
+
+        File webviewCacheDir = new File(getCacheDir().getAbsolutePath()+"/webviewCache");
+        Log.e(TAG, "webviewCacheDir path="+webviewCacheDir.getAbsolutePath());
+
+        //删除webview 缓存目录
+        if(webviewCacheDir.exists()){
+            deleteFile(webviewCacheDir);
+        }
+        //删除webview 缓存 缓存目录
+        if(appCacheDir.exists()){
+            deleteFile(appCacheDir);
+        }
+    }
+
+    /**
+     * 递归删除 文件/文件夹
+     *
+     * @param file
+     */
+    public void deleteFile(File file) {
+
+        Log.i(TAG, "delete file path=" + file.getAbsolutePath());
+
+        if (file.exists()) {
+            if (file.isFile()) {
+                file.delete();
+            } else if (file.isDirectory()) {
+                File files[] = file.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    deleteFile(files[i]);
+                }
+            }
+            file.delete();
+        } else {
+            Log.e(TAG, "delete file no exists " + file.getAbsolutePath());
+        }
+    }
+
+}
